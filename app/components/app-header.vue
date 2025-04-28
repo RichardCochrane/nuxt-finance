8aab685ede95ed454ece4374355f751253b5860b
Richard Cochrane <screamer42@gmail.com>
2025-04-28 19:05:56 +0200

    Initial project setup

---
+++ app/components/app-header.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { UAvatar } from '#components';
+
+</script>
+
+<template>
+    <header class="flex justify-between items-center">
+        <NuxtLink to="/" class="text-xl font-bold">
+            Finance Tracker
+        </NuxtLink>
+        <div>
+            <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+        </div>
+    </header>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    darkMode: 'class',
+    content: [
+        "./**/*.{js,tx,tsx,vue}"
+    ],
+    plugins: [ ]
+}
